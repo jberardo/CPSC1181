b5ced28ed0e394bf1c25ff91ea22e98278e900df
jberardo <kajoca@gmail.com>
2017-06-29 14:31:48 -0700

    after midterm

--- workspace/test/src/logz/UseLogger.java
+++ workspace/test/src/logz/UseLogger.java
@@ -16,16 +16,16 @@ public class UseLogger {
 
         // set the LogLevel to Severe, only severe Messages will be written
         LOGGER.setLevel(Level.SEVERE);
-        LOGGER.severe("Info Log");
-        LOGGER.warning("Info Log");
+        LOGGER.severe("Severe Log");
+        LOGGER.warning("Warning Log");
         LOGGER.info("Info Log");
         LOGGER.finest("Really not important");
 
         // set the LogLevel to Info, severe, warning and info will be written
         // finest is still not written
         LOGGER.setLevel(Level.INFO);
-        LOGGER.severe("Info Log");
-        LOGGER.warning("Info Log");
+        LOGGER.severe("Severe Log");
+        LOGGER.warning("Warning Log");
         LOGGER.info("Info Log");
         LOGGER.finest("Really not important");
     }
